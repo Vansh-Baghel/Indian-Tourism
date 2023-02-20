aa524fdc32793e214b9758b2d542b517e739bf64
Vansh Baghel <vanshbaghel912@gmail.com>
2023-02-20 15:24:50 +0530

    D

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
   useNewUrlParser: true,
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App running from port ${port}`);
