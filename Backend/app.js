335c1c9c68d6f1b8afbb73794581c15819ad7a8b
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-12-11 18:16:54 +0530

    Fixes

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,10 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const app = express();
-const morgan = require('morgan');
 const tourRouter = require('./routes/tourRouter');
 const AppError = require('./utils/appError');
 const cors = require('cors');
 
-app.use(morgan('dev'));
-// Used to get resolve the cors error.
 app.use(cors());
-
-// Safety measures
-// 1. Limiting requests.
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 100, // Setting for 1 hour
-  message: 'Too many requests from this IP, please try again after an hour!',
-});
 
 app.use('/api/v1/tours', tourRouter);
 
